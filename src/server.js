03e2296592972bcddfd1afeb5fc37e330d161685
Jonathan Feitor <jonathan.feitor@hotmail.com>
2023-06-06 10:54:11 -0300

    My first commit

---
+++ src/server.js
@@ -0,0 +1,45 @@
+const port = 3003;
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const database = require('./database');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+}));
+
+app.get('/products', (req, res, next) => {
+    res.send(database.getProducts());
+});
+
+app.get('/products/:id', (req, res, next) => {
+    res.send(database.getProduct(req.params.id));
+});
+
+app.post('/products', (req, res, next) => {
+    const product = database.saveProduct({
+        name: req.body.name,
+        pricers: req.body.price
+    });
+    res.send(product); // Gerar um JSON
+});
+
+app.put('/products/:id', (req, res, next) => {
+    const product = database.saveProduct({
+        id: req.params.id,
+        name: req.body.name,
+        price: req.body.price
+    });
+    res.send(product);
+});
+
+app.delete('/products/:id', (req, res, next) => {
+    const product = database.deleteProduct(req.params.id);
+    res.send(product);
+});
+
+app.listen(port, () => {
+    console.log(`Server is executing at port ${port}.`);
+});
